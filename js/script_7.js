f996ee3a0ba1270702c1bb5927ee915727f9b9aa
phbord <phbord@gmail.com>
2021-05-27 16:09:43 +0200

    fini

--- js/script_7.js
+++ js/script_7.js
@@ -1,2 +1,21 @@
 //Acné-Bot
 console.log('-------------------------------------------\n');
+let botResponse = function(str) {
+    if(str.slice(-1) === '?') {
+        return 'Ouais Ouais...';
+    }
+    else if(str && str === str.toUpperCase()) {
+        return 'Pwa, calme-toi...';
+    }
+    else if(str.toLowerCase().includes('fortnite')) {
+        return 'on s\'fait une partie soum-soum ?';
+    }
+    else if(!str || str == null || str === '') {
+        return 't\'es en PLS ?';
+    }
+    else {
+        return 'balek';
+    }
+}
+let response = prompt("Que puis-je faire pour t'aider ?");
+console.log(botResponse(response));
